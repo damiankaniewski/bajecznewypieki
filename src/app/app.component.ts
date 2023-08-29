849684c587a9c9dde0690665368b4a934af59548
Damian <daba9@op.pl>
2023-08-29 20:43:00 +0200

    navbar and some animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 
 export class AppComponent {
   title = 'bajeczne-wypieki';
+
+      showScrollToTopButton: boolean = false;
+
+    @HostListener('window:scroll', ['$event'])
+    onWindowScroll() {
+        this.showScrollToTopButton = window.scrollY > 300;
+    }
+
+    scrollToTop() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
 }
