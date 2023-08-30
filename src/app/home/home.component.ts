863046cdf07e09d496cb974034d079d049c86a06
Damian <daba9@op.pl>
2023-08-30 11:12:58 +0200

    home component updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  componentLoaded: boolean = false;
 
+  ngOnInit() {
+    setTimeout(() => {
+      this.componentLoaded = true;
+    }, 0);
+  }
 }
