1227594a8261d078fec045e3bfa85dcf6a0b2d7b
Damian <daba9@op.pl>
2023-10-27 18:55:21 +0200

    header update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.checkActiveRoute(event.url);
+      }
+    });
+  }
 
+  activeRoute: string = '';
+
+  checkActiveRoute(url: string) {
+    const routes = ['home', 'cakes', 'sweets', 'about', 'contact'];
+    for (const route of routes) {
+      if (url.includes('/' + route)) {
+        this.activeRoute = route;
+        return;
+      }
+    }
+    this.activeRoute = '';
+  }
+  
 }
