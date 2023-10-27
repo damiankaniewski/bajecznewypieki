3bf4bbf6f4f6e21c287fa11dc3b86b7fc91d41ad
Damian <daba9@op.pl>
2023-10-27 20:22:47 +0200

    main page updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  ngOnInit() {
+    // You can access the DOM element and update its styles here
+    const imageElement = document.querySelector('.main-photo') as HTMLElement;
+    const centeredContainerElement = document.querySelector('.centered-container') as HTMLElement;
+    if (imageElement) {
+      
+      setTimeout(() => {
+        imageElement.style.opacity = '1';
+        imageElement.style.filter = 'blur(10px) brightness(0.5)';
+      }, 100);
+      setTimeout(() => {
+        centeredContainerElement.style.opacity = '1';
+      }, 1000);
+    }
+  }
 }
