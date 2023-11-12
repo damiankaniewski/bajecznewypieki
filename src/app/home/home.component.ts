feda6a0157eb987803e9b6230e4a7a2807ab0536
Damian <daba9@op.pl>
2023-11-12 22:00:58 +0100

    home updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  imagePaths = {
+    'first': [
+      'assets/images/home/cakes/first/image1.jpg',
+      'assets/images/home/cakes/first/image2.jpg',
+      'assets/images/home/cakes/first/image3.jpg',
+      'assets/images/home/cakes/first/image4.jpg'
+    ],
+    'second': [
+      'assets/images/home/cakes/second/image1.jpg',
+      'assets/images/home/cakes/second/image2.jpg',
+      'assets/images/home/cakes/second/image3.jpg',
+      'assets/images/home/cakes/second/image4.jpg'
+    ],
+    'third': [
+      'assets/images/home/cakes/third/image1.jpg',
+      'assets/images/home/cakes/third/image2.jpg',
+      'assets/images/home/cakes/third/image3.jpg',
+      'assets/images/home/cakes/third/image4.jpg'
+    ],
+    'forth': [
+      'assets/images/home/cakes/forth/image1.jpg',
+      'assets/images/home/cakes/forth/image2.jpg',
+      'assets/images/home/cakes/forth/image3.jpg',
+      'assets/images/home/cakes/forth/image4.jpg'
+    ]
+  };
+
+  currentImageIndex: number = 0;
+
   ngOnInit() {
     const imageElement = document.querySelector('.main-photo') as HTMLElement;
     const centeredContainerElement = document.querySelector('.centered-container') as HTMLElement;
@@ -19,32 +49,13 @@ export class HomeComponent implements OnInit {
         centeredContainerElement.style.opacity = '1';
       }, 1000);
     }
+
+    this.startImageSlideshow();
   }
 
-  public currentImages: Record<string, number> = {
-    first: 1,
-    second: 1,
-    third: 1,
-    forth: 1
-  };
-
-  public getImagePath(folder: string): string {
-    return `assets/images/home/cakes/${folder}/image${this.currentImages[folder]}.jpg`;
-  }
-
-  public changeImage(folder: string): void {
-    const image = document.querySelector(`#${folder} img`);
-    if(image){
-      image.classList.add("fade-invisible");
-    }
-    setTimeout(() => {
-      this.currentImages[folder] = (this.currentImages[folder] % 4) + 1;
-      setTimeout(() => {
-        if(image){
-          image.classList.remove("fade-invisible");
-        }
-      },50)
-    }, 300)
-    
+  startImageSlideshow() {
+    setInterval(() => {
+      this.currentImageIndex = (this.currentImageIndex + 1) % 4;
+    }, 5000);
   }
 }
